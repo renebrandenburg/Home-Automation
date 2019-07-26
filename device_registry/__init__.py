d6bf65a6481b8f233f0eadacf291dee5fbe07456
René Brandenburg <rbrandenburg@Renes-MacBook-Pro.local>
2019-07-26 19:14:14 +0200

    Update requirements.txt

--- device_registry/__init__.py
+++ device_registry/__init__.py
@@ -1,8 +1,22 @@
+import markdown 
+
+# Import the framewrok 
 from flask import Flask 
 
+# Create an instance of Flask 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+@app.route("/")
+def index():
+	"""Present some documentation """
+
+
+	# OPen the README fiel 
+	with open(os.path.dirname(app.root_path) +'/README.md', 'r') as markdown_file:
+
+
+		# Read the content of the file 
+		content = markdown_file.read()
+
+		# Convert to HTML 
+		return markdown.markdown(content)
